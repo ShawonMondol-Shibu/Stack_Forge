d44c0371ba382c1327bd3325f979b45d0ba684c0
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2026-08-11 21:00:29 +0600

    (dashobard/profile): skills section added

--- src/app/dashboard/profile/about/page.tsx
+++ src/app/dashboard/profile/about/page.tsx
@@ -1,10 +1,12 @@
 import AboutMe from '@/components/Shared/dashboard/profile/about/AboutMe';
+import Skills from '@/components/Shared/dashboard/profile/about/Skills';
 import React from 'react'
 
 export default function Page() {
   return (
-    <div className={"w-4xl grid grid-cols-2 gap-4 items-center justify-center"}>
+    <div className={"w-4xl grid grid-cols-2 gap-4 items-start justify-center"}>
         <AboutMe/>
+        <Skills/>
     </div>
   )
 }
---
+++ src/components/Shared/dashboard/profile/about/Skills.tsx
@@ -0,0 +1,57 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Item,
+  ItemContent,
+  ItemGroup,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { Pencil } from "lucide-react";
+import React from "react";
+import { SiNestjs } from "react-icons/si";
+
+export default function Skills() {
+  return (
+    <Card className={"gap-0"}>
+      <CardHeader>
+        <CardTitle>Skills</CardTitle>
+        <CardAction>
+          <Button size={"icon-sm"}>
+            <Pencil />
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <ItemGroup>
+          <div className="flex flex-row flex-wrap items-center gap-2">
+            {Array.from({ length: 5 }, (_, i) => (
+              <Item
+                key={i}
+                variant={"outline"}
+                size={"xs"}
+                className={"w-fit p-2"}
+              >
+                <ItemMedia>
+                  <SiNestjs />
+                </ItemMedia>
+                <ItemContent>
+                  <ItemTitle>NestJS</ItemTitle>
+                </ItemContent>
+              </Item>
+            ))}
+          </div>
+        <Button variant={"link"} size={"xs"}>
+            View all skills
+        </Button>
+        </ItemGroup>
+      </CardContent>
+    </Card>
+  );
+}
