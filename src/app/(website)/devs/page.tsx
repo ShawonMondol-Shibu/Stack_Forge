e3b8fef008a2035aff490638fce002abd7c176c9
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2026-08-29 19:03:32 +0600

    devs page updated

--- src/app/(website)/devs/page.tsx
+++ src/app/(website)/devs/page.tsx
@@ -1,22 +1,24 @@
 "use client";
 import ProfileCard from "@/components/Shared/website/ProfileCard";
 import { apiService } from "@/lib/api-routes/apis";
+import { ApiResponse } from "@/lib/types/api";
 import { UserProfile } from "@/lib/types/profile-type";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
   const { data: profiles } = useQuery({
     queryKey: ["profiles"],
-    queryFn: ()=> apiService("profile/all"),
+    queryFn: () =>
+      apiService<ApiResponse<UserProfile[]>>({ endpoint: "profile/all" }),
+    select: (data) => data.data,
   });
 
   return (
     <div className="h-dvh py-10 mt-10">
       <div className="flex items-center gap-6 justify-start">
-
-      {profiles?.data.map((profile:UserProfile,i:number) => (
-        <ProfileCard key={i} profile={profile} />
-      ))}
+        {profiles?.map((profile: UserProfile, i: number) => (
+          <ProfileCard key={i} profile={profile} />
+        ))}
       </div>
     </div>
   );
